80dc70c0533e5b9fff6a6d706ac211024f239922
Ioan Gogozan <nelutugogozan@yahoo.com>
2023-08-17 20:31:38 +0200

    Add POST and DELETE recommendations handlers

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -4,6 +4,8 @@ var express = require('express');
 var router = express.Router();
 const fs = require("fs")
 const path = require("path")
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,6 +20,26 @@ router.post('/', function(req, res, next) {
   currentData.push({ avatar, name, role, description });
   fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(currentData, null, 4));
   res.redirect('/recommendations');
+});
+
+router.post('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let recommendationsArray = JSON.parse(rawdata);
+  if(recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
+    const newArray = recommendationsArray.concat([req.body])
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  }
+  res.end();
+});
+
+router.delete('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let recommendationsArray = JSON.parse(rawdata);
+  const newArray = recommendationsArray.filter(x => x.name !== req.body.name)
+  if (newArray.length !== recommendationsArray.length ) {
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  }
+  res.end();
 });
 
 
