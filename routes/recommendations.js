7341d39831c383b69e695fc13c7a1591509b7ad1
Ioan Gogozan <nelutugogozan@yahoo.com>
2023-08-17 19:38:54 +0200

    Refactor recommendations

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -1,9 +1,24 @@
+
+
 var express = require('express');
 var router = express.Router();
+const fs = require("fs")
+const path = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('recommendations');
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  res.render('recommendations', { data: JSON.parse(data)
+ });
 });
+
+router.post('/', function(req, res, next) {
+  const { avatar, name, role, description } = req.body;
+  const currentData = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json")));
+  currentData.push({ avatar, name, role, description });
+  fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(currentData, null, 4));
+  res.redirect('/recommendations');
+});
+
 
 module.exports = router;
